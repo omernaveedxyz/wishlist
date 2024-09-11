718c207d365b305fcc03b09917a6a155a0b4ea36
Omer Naveed <me@omernaveed.dev>
2024-09-10 23:22:40 -0500

    src/lib/types/formSchema.ts: Set min/max for price

--- src/lib/types/formSchema.ts
+++ src/lib/types/formSchema.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const formSchema = z.object({
 	title: z.string().min(1).max(256),
 	link: z.string().url(),
-	price: z.coerce.number().nonnegative().finite()
+	price: z.coerce.number().gte(0).lte(1000000)
 });
